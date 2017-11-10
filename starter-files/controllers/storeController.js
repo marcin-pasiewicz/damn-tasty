507d83d20d9668efd72b46a2d866464eb6b30ec0
marcin-pasiewicz <marcin.pasiewicz@gmail.com>
2017-11-10 20:46:20 +0100

    remove useless files

--- starter-files/controllers/storeController.js
+++
@@ -1,8 +0,0 @@
-exports.myMiddleware = (req, res, next) => {
-    req.name = 'Marcin';
-    next()
-};
-
-exports.homePage = (req, res) => {
-    res.render('index');
-};
--- starter-files/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const storeController = require('../controllers/storeController')
-
-// Do work here
-router.get('/', storeController.myMiddleware, storeController.homePage);
-
-module.exports = router;
